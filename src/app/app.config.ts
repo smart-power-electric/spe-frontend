bdc553e3aede1387ea574abae8682f3e63282eda
raymelguerra <raymelguerra@gmail.com>
2024-10-24 12:18:42 -0600

    First Commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(), provideAnimationsAsync(),
+  ],
+};
